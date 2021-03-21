3276f9fd43ae6dcb3ac50b6df022e322a16738dc
Aki <kydovQWR@gmail.com>
2021-03-21 23:10:09 +0900

    動作できるようになったためコミット。

--- README.md
+++ README.md
@@ -14,9 +14,9 @@
 
 ## Setup
 
-（記載中）
+（記載中。ハードのセットアップを記載する。）
 
-```latex{cmd=true, hide=false}
+```latex{cmd=true, hide=true}
 \documentclass{standalone}
 \usepackage[siunitx,american]{circuitikz}
 \begin{document}
@@ -28,8 +28,7 @@
 \end{document}
 ```
 
-
-```latex{cmd=true hide=false}
+```latex{cmd=true hide=true}
 \documentclass{standalone}
 \usepackage[siunitx, RPvoltages]{circuitikz}
 \begin{document}
@@ -48,9 +47,50 @@
 \end{document}
 ```
 
+続いて、本リポジトリのファイルを任意の場所に配置する（例：/home/user01/TapTapに配置。）
+
+次に、起動時にスクリプトを実行させるため、/etc/rc.localを編集する。
+
+```cmd
+sudo vi /etc/rc.local
+```
+
+/etc/rc.localを変更していない場合、以下のように記述されている。
+
+```shell
+# Print the IP address
+_IP=$(hostname -I) || true
+if [ "$_IP" ]; then
+  printf "My IP address is %s\n" "$_IP"
+fi
+
+exit 0
+```
+
+/etc/rc.localへ以下のように追記する。
+
+```shell
+# Print the IP address
+_IP=$(hostname -I) || true
+if [ "$_IP" ]; then
+  printf "My IP address is %s\n" "$_IP"
+fi
+
+cd /home/user01/TapTap
+nohup node taptap.js &
+
+exit 0
+```
+
+修正後、以下のコマンドでrc.localのエラーをチェックする。
+
+```cmd
+sudo /etc/rc.local
+```
+
 ## Usage
 
-（記載中）
+（記載中。タクトスイッチ押下で動かす旨を記載する。）
 
 ## License
 
@@ -84,6 +124,10 @@
 1. @Gadgetoid, Raspberry Pi Pinout, Pinout.xyz, -, <https://pinout.xyz/>
 1. shuichi, ラズパイ：タクトスイッチでPythonプログラムを動作させる, 人生は読めないブログ, 2020/08/16, <https://torisky.com/%E3%83%A9%E3%82%BA%E3%83%91%E3%82%A4%EF%BC%9A%E3%82%BF%E3%82%AF%E3%83%88%E3%82%B9%E3%82%A4%E3%83%83%E3%83%81%E3%81%A7python%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%A0%E3%82%92%E5%8B%95%E4%BD%9C/>
 1. @K-Ponta, Raspberry Pi 3 B+に シャットダウンスイッチを付ける, Qiita, 2019/04/14, <https://qiita.com/K-Ponta/items/12127d7077d69a82693c>
+1. 文系の伊藤さん, タクトスイッチは向きに注意, 文系の伊藤さんと電気の話, 2017/03/25, <https://ameblo.jp/bun-ito/entry-12259611903.html>
 1. サンダー, ブレッドボードの使い方【ブレッドボードでLEDを光らせてみよう】, THUNDER BLOG, 2020/05/28, <https://thunderblog.org/2019/03/bread_board.html>
+1. しなぷす, 【初心者向け】ブレッドボードとタクトスイッチで論理回路を作る(5), 京都しなぷすのハード制作日誌, 2020/05/14, <https://synapse.kyoto/hard/switch-logic/page005.html>
+1. furoblog, 【JavaScript】日付処理で意識するべきこと, furoblog’s blog, 2019/04/25, <https://furoblog.hatenablog.com/entry/js-datediff>
+1. Carlos Delgado, How to shutdown and reboot Linux with Node.js, OUR CODE WORLD, 2017/03/14, <https://ourcodeworld.com/articles/read/411/how-to-shutdown-and-reboot-linux-with-node-js>
 1. からあげ (id:karaage) , READMEの良さそうな書き方・テンプレート【GitHub/Bitbucket】, からあげ, 2018/01/19, <https://karaage.hatenadiary.jp/entry/2018/01/19/073000>
 1. matiassingers, Awesome README, github.com, 2020/02/28, <https://github.com/matiassingers/awesome-readme>
--- taptap.js
+++ taptap.js
@@ -10,20 +10,24 @@
  * @license WTFPL-2.0
  */
 const gpio = require('rpi-gpio');
+const { exec } = require('child_process');
 
+//========================================================
+// 各種データを定義する。
+//========================================================
 /**
- * シャットダウン実行用スイッチに関する情報定義オブジェクト。
+ * シャットダウン実行用タクトスイッチに関する情報定義オブジェクト。
  * @property {Number}  pin       シャットダウン実行用のタクトスイッチを接続するピン番号。
  * @property {Boolean} state.on  タクトスイッチが押下されていることを示すブール値。
- * @property {Boolean} state.off タクトスイッチが押下されていないことを示すブール値。
+ * @property {Boolean} state.off タクトスイッチが離上されている（押下されていない）ことを示すブール値。
  */
  const Data_ShutdownControlSwitch = {
-  pin: 36,
+  pin: 22,
   state: { on: true, off: false },
 };
 
 /**
- * リレーループ制御用スイッチに関する情報定義オブジェクト。
+ * リレーループ制御用タクトスイッチに関する情報定義オブジェクト。
  * @property {Number}  pin                 リレーループ制御用のタクトスイッチを接続するピン番号。
  * @property {Boolean} state.on            タクトスイッチが押下されていることを示すブール値。
  * @property {Boolean} state.off           タクトスイッチが押下されていないことを示すブール値。
@@ -55,27 +59,44 @@ const Data_RelayTouchBoard = {
  * @class
  */
 class RelayLooper {
-  constructor(aRelayTimePeriod, aRelayTouchBoardState, aRelayLoopState) {
+  /**
+   * @constructor
+   * @param {Number} aRelayTimePeriod              リレー期間を示すData_RelayTouchBoard.timePeriodの値。デフォルト値はoff。
+   * @param {Boolean} aRelayTouchBoardState        リレーの有効/無効を示すData_RelayTouchBoard.stateの値。デフォルト値はoff。
+   * @param {Boolean} aRelayLoopControlSwitchState リレー制御用タクトスイッチの押下/離上を示すData_RelayLoopControlSwitch.stateの値。デフォルト値はoff。
+   * @param {Boolean} aRelayLoopState              Data_RelayLoopControlSwitch.relayLoopStateの値。デフォルト値はstop。
+   */
+  constructor(
+    aRelayTimePeriod = Data_RelayTouchBoard.timePeriod.off,
+    aRelayTouchBoardState = Data_RelayTouchBoard.state.off,
+    aRelayLoopControlSwitchState = Data_RelayLoopControlSwitch.state.off,
+    aRelayLoopState = Data_RelayLoopControlSwitch.relayLoopState.stop) {
    /**
-    * リレーを実行するタイマーを割り当てる一次変数。
+    * リレーを実行するタイマーを割り当てるプロパティ。
     * @type {Object}
     */
     this.relayTimer;
 
     /**
-     * リレー有効時間、またはリレー無効時間を割り当てる一次変数。
+     * リレー有効時間、またはリレー無効時間を割り当てるプロパティ。
      * @type {Object}
      */
     this.relayTimePeriod = aRelayTimePeriod;
 
     /**
-     * リレーが有効/無効であるかを割り当てる一次変数。
+     * リレーが有効/無効であるかを割り当てるプロパティ。
      * @type {Boolean}
      */
     this.relayTouchBoardState = aRelayTouchBoardState;
 
     /**
-     * リレーループが有効/無効であるかを割り当てる一次変数。
+     * リレー制御用タクトスイッチが押下されているかを示すプロパティ。
+     * @type {Boolean}
+     */
+    this.relayLoopControlSwitchState = aRelayLoopControlSwitchState;
+
+    /**
+     * リレーループが有効/無効であるかを割り当てるプロパティ。
      * @type {Boolean}
      */
     this.relayLoopState = aRelayLoopState;
@@ -85,24 +106,23 @@ class RelayLooper {
      * @returns なし。
      */
     this.runRelayLoop = () => {
-      console.log(`runRelayLoop()`);
       // リレーループが停止中であれば何もしない。
-      // if (this.relayLoopState === Data_RelayLoopControlSwitch.relayLoopState.stop) {
-      //   return ;
-      // }
+      if (this.relayLoopState === Data_RelayLoopControlSwitch.relayLoopState.stop) {
+        return ;
+      }
 
-      // // リレーオフであればリレーオンにして、リレー有効時間経過後に再度runRelayLoop()を呼び出すようにタイマーを設定する。
-      // // リレーオンであればリレーオフにして、リレー無効時間経過後に再度runRelayLoop()を呼び出すようにタイマーを設定する。
-      // if (this.relayTouchBoardState === Data_RelayTouchBoard.state.off) {
-      //   this.relayTouchBoardState = Data_RelayTouchBoard.state.on;
-      //   this.relayTimePeriod = Data_RelayTouchBoard.timePeriod.on;
-      // } else {
-      //   this.relayTouchBoardState = Data_RelayTouchBoard.state.off;
-      //   this.relayTimePeriod = Data_RelayTouchBoard.timePeriod.off;
-      // }
+      // リレーオフであればリレーオンにして、リレー有効時間経過後に再度runRelayLoop()を呼び出すようにタイマーを設定する。
+      // リレーオンであればリレーオフにして、リレー無効時間経過後に再度runRelayLoop()を呼び出すようにタイマーを設定する。
+      if (this.relayTouchBoardState === Data_RelayTouchBoard.state.off) {
+        this.relayTouchBoardState = Data_RelayTouchBoard.state.on;
+        this.relayTimePeriod = Data_RelayTouchBoard.timePeriod.on;
+      } else {
+        this.relayTouchBoardState = Data_RelayTouchBoard.state.off;
+        this.relayTimePeriod = Data_RelayTouchBoard.timePeriod.off;
+      }
 
-      // gpio.write(Data_RelayTouchBoard.pin, this.relayTouchBoardState);
-      // this.relayTimer = setTimeout(this.runRelayLoop, this.relayTimePeriod);
+      gpio.write(Data_RelayTouchBoard.pin, this.relayTouchBoardState);
+      this.relayTimer = setTimeout(this.runRelayLoop, this.relayTimePeriod);
     };
 
     /**
@@ -110,25 +130,122 @@ class RelayLooper {
      * @returns なし。
      */
     this.stopRelayLoop = () => {
-      console.log(`stopRelayLoop()`);
-      // this.relayTouchBoardState = Data_RelayTouchBoard.state.off;
-      // this.relayTimePeriod = Data_RelayTouchBoard.timePeriod.off;
+      // リレーオフにし、リレーループ用のタイマーをクリアする。
+      this.relayTouchBoardState = Data_RelayTouchBoard.state.off;
+      this.relayTimePeriod = Data_RelayTouchBoard.timePeriod.off;
 
-      // gpio.write(Data_RelayTouchBoard.pin, this.relayTouchBoardState);
-      // clearTimeout(this.relayTimer);
+      gpio.write(Data_RelayTouchBoard.pin, this.relayTouchBoardState);
+      clearTimeout(this.relayTimer);
     };
   }
 }
 
-// setTimeoutでループし、ONを検出すると状態を反転させる。動いているなら止め、止まっているなら動かす。
+//========================================================
+// リレータッチボードの出力を有効化する。
+//========================================================
 gpio.setup(Data_RelayTouchBoard.pin, gpio.DIR_OUT, () => {
-  console.log(`PIN${Data_RelayTouchBoard.pin}: OUT start.`);
+  // console.log(`PIN${Data_RelayTouchBoard.pin}: OUT start.`);
 });
 
+//========================================================
+// リレーループ制御用タクトスイッチの入力を有効化する。
+//========================================================
+/**
+ * リレーループ実行クラスをインスタンス化。
+ * @type {Object}
+ */
+let relayLooper = new RelayLooper();
+
+/**
+ * リレーループを実行するためのタイマー。
+ * @type {Object}
+ */
+let relayLooperTimer;
+
 gpio.setup(Data_RelayLoopControlSwitch.pin, gpio.DIR_IN, () => {
-  console.log(`PIN${Data_RelayLoopControlSwitch.pin}: IN start.`);
-  gpio.read(Data_RelayLoopControlSwitch.pin, (err, value) => {
-    console.log(`PIN${Data_RelayLoopControlSwitch.pin} value: ${value}`);
-  })
-  // RunRelayLoop();
+  const switchStateChecker = () => {
+    gpio.read(Data_RelayLoopControlSwitch.pin, (err, value) => {
+      // タクトスイッチが押下されると、リレーループ実行/停止をトグルする。
+      if (value === Data_RelayLoopControlSwitch.state.on) {
+
+        // すでにスイッチが押下中である場合、押しっぱなしとみなして無視する（スイッチがオフの場合に処理を実行する。）
+        if (relayLooper.relayLoopControlSwitchState === Data_RelayLoopControlSwitch.state.off) {
+          // リレーループ実行/停止をトグルする。
+          if (relayLooper.relayLoopState === Data_RelayLoopControlSwitch.relayLoopState.run) {
+            relayLooper.relayLoopState = Data_RelayLoopControlSwitch.relayLoopState.stop;
+            relayLooper.stopRelayLoop();
+          } else {
+            relayLooper.relayLoopState = Data_RelayLoopControlSwitch.relayLoopState.run;
+            relayLooper.runRelayLoop();
+          }
+        }
+
+        // タクトスイッチが押下された旨をプロパティへ反映する。
+        relayLooper.relayLoopControlSwitchState = Data_RelayLoopControlSwitch.state.on;
+      } else {
+        // タクトスイッチが離上された旨をプロパティへ反映する。
+        relayLooper.relayLoopControlSwitchState = Data_RelayLoopControlSwitch.state.off
+      }
+    });
+    relayLooperTimer = setTimeout(switchStateChecker, 100);
+  };
+  relayLooperTimer = setTimeout(switchStateChecker, 100);
+});
+
+//========================================================
+// シャットダウン実行用タクトスイッチの入力を有効化する。
+//========================================================
+/**
+ * シャットダウン用タクトスイッチが押下された時点の日付。
+ * @type {Date}
+ */
+let pushedDate = null;
+
+/**
+ * シャットダウンを開始しているかを示すブール値。trueならシャットダウンを開始しており、falseなら開始していない。
+ * @type {Boolean}
+ */
+let isShutdownStart = false;
+
+/**
+ * シャットダウンを実行するためのタイマー。
+ * @type {Object}
+ */
+let shutdownTimer;
+
+gpio.setup(Data_ShutdownControlSwitch.pin, gpio.DIR_IN, () => {
+  const switchStateChecker = () => {
+    gpio.read(Data_ShutdownControlSwitch.pin, (err, value) => {
+      // タクトスイッチが5秒間連続で押下されると、シャットダウンを実行する。
+      if (isShutdownStart === false) {
+        if (value === Data_ShutdownControlSwitch.state.on) {
+          // スイッチを押下した時点の日時を保存する。
+          if (pushedDate === null) {
+            pushedDate = new Date();
+          }
+          const currentDate = new Date();
+          const pushedPeriod = currentDate.getTime() - pushedDate.getTime();
+
+          if (pushedPeriod > 5000) {
+            isShutdownStart = true;
+            const poweroff = exec('sudo /usr/sbin/poweroff');
+
+            poweroff.stdout.on('data', (data) => {
+              console.log(`stdout: ${data}`);
+            });
+
+            poweroff.stderr.on('data', (data) => {
+              console.log(`stderr: ${data}`);
+            });
+          }
+        } else {
+          // 5秒経過せずにスイッチを離上すると、スイッチを押下した時点の日時をリセットする。
+          pushedDate = null;
+        }
+      }
+    });
+    shutdownTimer = setTimeout(switchStateChecker, 100);
+  };
+
+  shutdownTimer = setTimeout(switchStateChecker, 100);
 });
